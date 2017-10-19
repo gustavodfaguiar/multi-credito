411cfd79093943d944e0597dd5ad96203ee042c1
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-19 21:47:39 -0200

    create method get one user

--- multi_credit/user/views.py
+++ multi_credit/user/views.py
@@ -2,9 +2,22 @@ from multi_credit.db import db
 from flask import request, jsonify, Blueprint
 from multi_credit.user.models import User
 from werkzeug.security import generate_password_hash
+from multi_credit.security import token_required
 
 
 user = Blueprint('user', __name__)
+
+
+# GET /user/<int:user_id>
+@user.route('/v1/user/<int:user_id>', methods=['GET'])
+@token_required
+def get_one_user(current_user, user_id):
+    user = User.query.filter_by(id=user_id).first()
+
+    if not user:
+        return jsonify({'message': 'No user found!'})
+
+    return jsonify({'user': user.serialize})
 
 
 # POST /user data: {name, email, password}
