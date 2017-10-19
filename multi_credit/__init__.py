4895ba5bcb02de9f5688d4305d356e714ebcbc68
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-19 17:11:07 -0200

    added blueprint user

--- multi_credit/__init__.py
+++ multi_credit/__init__.py
@@ -1,12 +1,14 @@
 import os
 from multi_credit.db import db
 from flask import Flask
+from multi_credit.user.views import user
 
 
 app = Flask(__name__)
 app.config.from_object(
     os.environ.get('APP_SETTINGS', 'multi_credit.config.DevelopmentConfig'))
 
+app.register_blueprint(user, url_prefix='/api')
 
 with app.app_context():
     db.init_app(app)
