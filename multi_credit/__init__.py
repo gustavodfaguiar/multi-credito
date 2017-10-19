5842ec346c440fb92d39cfd56d6380dcc781051b
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-19 16:47:25 -0200

    Added database configuration

--- multi_credit/__init__.py
+++ multi_credit/__init__.py
@@ -1,8 +1,11 @@
+import os
 from multi_credit.db import db
 from flask import Flask
 
 
 app = Flask(__name__)
+app.config.from_object(
+    os.environ.get('APP_SETTINGS', 'multi_credit.config.DevelopmentConfig'))
 
 
 with app.app_context():
---
+++ multi_credit/config.py
@@ -0,0 +1,21 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secreta')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'postgres://postgres@127.0.0.1/db')
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'sqlite:///multi_credit.db')
