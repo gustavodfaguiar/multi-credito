b2f6c16b82c318a95007f10e488de4fcb9bb58cb
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-20 15:09:55 -0200

    add method create card

--- multi_credit/__init__.py
+++ multi_credit/__init__.py
@@ -3,6 +3,7 @@ from multi_credit.db import db
 from flask import Flask
 from multi_credit.user.views import user
 from multi_credit.wallet.views import wallet
+from multi_credit.card.views import card
 from multi_credit.admin.views import login
 
 
@@ -12,6 +13,7 @@ app.config.from_object(
 
 app.register_blueprint(wallet, url_prefix='/api')
 app.register_blueprint(user, url_prefix='/api')
+app.register_blueprint(card, url_prefix='/api')
 app.register_blueprint(login, url_prefix='/api')
 
 with app.app_context():
---
+++ multi_credit/card/views.py
@@ -0,0 +1,46 @@
+from multi_credit.db import db
+from flask import request, jsonify, Blueprint
+from multi_credit.card.models import Card
+from multi_credit.wallet.models import Wallet
+from multi_credit.security import token_required
+from datetime import datetime
+
+
+card = Blueprint('card', __name__)
+
+
+# POST /card data: {number, expiration_date, validity_date,
+#            name, cvv, limit, credit, wallet_id}
+@card.route("/v1/card", methods=['POST'])
+@token_required
+def create_card(current_user):
+    wallet = Wallet.query.filter_by(user_id=current_user.id).first()
+
+    if not wallet:
+        return jsonify({'message': 'No wallet found!'})
+
+    request_data = request.get_json()
+    format_expiration_date = datetime.strptime(
+        request_data['expiration_date'].replace("-", ""), "%Y%m%d").date()
+
+    format_validity_date = datetime.strptime(
+        request_data['expiration_date'].replace("-", ""), "%Y%m%d").date()
+
+    new_card = Card(
+        number=request_data['number'],
+        expiration_date=format_expiration_date,
+        validity_date=format_validity_date,
+        name=current_user.name_card,
+        cvv=request_data['cvv'],
+        limit=request_data['limit'],
+        credit=request_data['limit'],
+        wallet_id=wallet.id
+    )
+
+    try:
+        db.session.add(new_card)
+        db.session.commit()
+    except:
+        return jsonify({'message': 'Card already exists!'}), 200
+
+    return jsonify({'message': 'New card created!'}), 201
