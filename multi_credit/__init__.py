aabac1e75f607d304d7e05dd45cb1a70fcf15cb8
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-20 00:17:43 -0200

    create method post api wallet

--- multi_credit/__init__.py
+++ multi_credit/__init__.py
@@ -2,6 +2,7 @@ import os
 from multi_credit.db import db
 from flask import Flask
 from multi_credit.user.views import user
+from multi_credit.wallet.views import wallet
 from multi_credit.admin.views import login
 
 
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object(
     os.environ.get('APP_SETTINGS', 'multi_credit.config.DevelopmentConfig'))
 
+app.register_blueprint(wallet, url_prefix='/api')
 app.register_blueprint(user, url_prefix='/api')
 app.register_blueprint(login, url_prefix='/api')
 
---
+++ multi_credit/wallet/views.py
@@ -0,0 +1,28 @@
+from multi_credit.db import db
+from flask import jsonify, Blueprint
+from multi_credit.wallet.models import Wallet
+from multi_credit.security import token_required
+
+
+wallet = Blueprint('wallet', __name__)
+
+
+# POST /wallet
+@wallet.route("/v1/wallet", methods=['POST'])
+@token_required
+def create_wallet(current_user):
+
+    new_wallet = Wallet(
+        max_limit=0,
+        user_limit=0,
+        credit=0,
+        user_id=current_user.id
+    )
+
+    try:
+        db.session.add(new_wallet)
+        db.session.commit()
+    except:
+        return jsonify({'message': 'Wallet already exists!'})
+
+    return jsonify({'message': 'New wallet created!'})
