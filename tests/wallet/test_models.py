855f705d84e5a17d8b90502b666a9a21e767b207
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-21 11:09:18 -0200

    add tearDown function and wallet tests update

--- tests/wallet/test_models.py
+++ tests/wallet/test_models.py
@@ -3,7 +3,7 @@ import unittest
 import json
 
 
-class UserModelsTestCase(unittest.TestCase):
+class WalletModelsTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.app.config.from_object('multi_credit.config.TestingConfig')
@@ -44,8 +44,13 @@ class UserModelsTestCase(unittest.TestCase):
         with self.app.app_context():
             db.create_all()
 
+    def tearDown(self):
+        with self.app.app_context():
+            db.drop_all()
+            db.create_all()
+
     def test_create_wallet(self):
-        response_create = self.client.post(
+        self.client.post(
             '/api/v1/user',
             data=json.dumps(self.user_data),
             headers=self.headers)
@@ -68,6 +73,10 @@ class UserModelsTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 201)
 
     def test_registered_with_already_registered_wallet(self):
+        self.client.post(
+            '/api/v1/user',
+            data=json.dumps(self.user_data),
+            headers=self.headers)
 
         response_login = self.client.get(
             '/api/v1/login',
@@ -76,10 +85,16 @@ class UserModelsTestCase(unittest.TestCase):
         auth_token = json.loads(response_login.data.decode())
         self.headers['x-access-token'] = auth_token['token']
 
+        self.client.post(
+            '/api/v1/wallet',
+            data=json.dumps(self.wallet_data),
+            headers=self.headers)
+
         response = self.client.post(
             '/api/v1/wallet',
             data=json.dumps(self.wallet_data),
             headers=self.headers)
+
         result = json.loads(response.data.decode())
 
         self.assertEqual(
@@ -87,6 +102,11 @@ class UserModelsTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_wallet(self):
+        self.client.post(
+            '/api/v1/user',
+            data=json.dumps(self.user_data),
+            headers=self.headers)
+
         response_login = self.client.get(
             '/api/v1/login',
             data=json.dumps(self.user_login),
@@ -94,9 +114,15 @@ class UserModelsTestCase(unittest.TestCase):
         auth_token = json.loads(response_login.data.decode())
         self.headers['x-access-token'] = auth_token['token']
 
+        self.client.post(
+            '/api/v1/wallet',
+            data=json.dumps(self.wallet_data),
+            headers=self.headers)
+
         response_wallet = self.client.get(
             '/api/v1/wallet',
             headers=self.headers)
         response_message = json.loads(response_wallet.data.decode())
         self.assertEqual(response_message, self.wallet_return)
         self.assertEqual(response_wallet.status_code, 201)
+
