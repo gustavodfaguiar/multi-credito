1a9957180a8f1c1d115f922af980bb7f7062303e
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-23 15:11:07 -0200

    remove route creating wallet

--- multi_credit/wallet/views.py
+++ multi_credit/wallet/views.py
@@ -1,4 +1,3 @@
-from multi_credit.db import db
 from flask import jsonify, Blueprint
 from multi_credit.wallet.models import Wallet
 from multi_credit.security import token_required
@@ -14,24 +13,3 @@ def get_wallet(current_user):
     wallet = Wallet.query.filter_by(user_id=current_user.id).first()
 
     return jsonify({'wallet': wallet.serialize}), 201
-
-
-# POST /wallet
-@wallet.route("/v1/wallet", methods=['POST'])
-@token_required
-def create_wallet(current_user):
-
-    new_wallet = Wallet(
-        max_limit=0,
-        user_limit=0,
-        credit=0,
-        user_id=current_user.id
-    )
-
-    try:
-        db.session.add(new_wallet)
-        db.session.commit()
-    except:
-        return jsonify({'message': 'Wallet already exists!'}), 200
-
-    return jsonify({'message': 'New wallet created!'}), 201
