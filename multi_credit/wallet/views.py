6a179280f7e7d31bc044c96c42da6d0a167be9c6
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-24 17:14:15 -0200

    add route to change limit

--- multi_credit/wallet/views.py
+++ multi_credit/wallet/views.py
@@ -1,5 +1,7 @@
-from flask import jsonify, Blueprint
+from multi_credit.db import db
+from flask import jsonify, Blueprint, request
 from multi_credit.wallet.models import Wallet
+from multi_credit.card.models import Card
 from multi_credit.security import token_required
 
 
@@ -13,3 +15,19 @@ def get_wallet(current_user):
     wallet = Wallet.query.filter_by(user_id=current_user.id).first()
 
     return jsonify({'wallet': wallet.serialize}), 201
+
+
+# PUT /wallet
+@wallet.route('/v1/wallet', methods=['PUT'])
+@token_required
+def update_limit_wallet(current_user):
+    wallet = Wallet.query.filter_by(
+        user_id=current_user.id).first()
+
+    if request.json.get('user_limit') > wallet.max_limit:
+        return jsonify({'message': "Passed the limit!"}), 200
+
+    wallet.user_limit = request.json.get('user_limit')
+    db.session.commit()
+
+    return jsonify({'message': "Updated limit wallet!"}), 201
