41d1f27d1f137499b6e6c35e973792622714d3fd
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-24 00:51:27 -0200

    change value of attribute expiration date

--- tests/card/test_unit.py
+++ tests/card/test_unit.py
@@ -9,7 +9,7 @@ class CardUnitModelsTestCase(unittest.TestCase):
         self.card_data = {
             "number": 5165834017829286,
             "expiration_date": "2018-07-5",
-            "validity_date": "2018-07-5",
+            "validity_date": 5,
             "cvv": 408,
             "limit": 2000,
             "credit": 1000
@@ -19,8 +19,8 @@ class CardUnitModelsTestCase(unittest.TestCase):
         card = Card()
         value_pay = 500
         card.credit = 1000
-        today = datetime.strptime("2017-05-7", "%Y-%m-%d")
-        date_validate = datetime.strptime("2017-05-7", "%Y-%m-%d")
+        today = datetime.strptime("2017-09-5", "%Y-%m-%d")
+        date_validate = 10
         credit = card.pay_card(
             value_pay, today, date_validate)
 
