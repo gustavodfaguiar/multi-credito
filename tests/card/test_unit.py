e294bcf3d0c4e04ea23fedf702b40bb54db92086
Gustavo Aguiar <gusttavoaguiarr@gmail.com>
2017-10-29 21:01:48 -0200

    change the card payment test

--- tests/card/test_unit.py
+++ tests/card/test_unit.py
@@ -4,20 +4,11 @@ from multi_credit.card.models import Card
 
 class CardUnitModelsTestCase(unittest.TestCase):
     def setUp(self):
-
-        self.card_data = {
-            "number": 5165834017829286,
-            "expiration_date": "2018-07-5",
-            "validity_date": 5,
-            "cvv": 408,
-            "limit": 2000,
-            "credit": 1000
-        }
+        self.card = Card()
 
     def test_pay_card(self):
-        card = Card()
         value_pay = 500
-        card.credit = 1000
-        credit = card.pay_card(value_pay)
+        self.card.credit = 1000
+        credit = self.card.pay_card(value_pay)
 
         self.assertEqual(credit, 1500)
